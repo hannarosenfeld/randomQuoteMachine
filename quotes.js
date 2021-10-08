22578abaef9b598994f30813f47e5b0a7384fdca
Hanna Rosenfeld <22946092+hannarosenfeld@users.noreply.github.com>
2021-10-08 12:56:32 -0400

    Add quotes.js

---
+++ quotes.js
@@ -0,0 +1,53 @@
+         const QUOTES = [
+             { quote: "Waste no more time arguing what a good man should be. Be One.",
+               author: "Marcus Aurelius"
+             },
+             { quote: "Think of the life you have lived until now as over and, as a dead man, see what’s left as a bonus and live it according to Nature. Love the hand that fate deals you and play it as your own, for what could be more fitting?",
+               author: "Marcus Aurelius"
+             },
+             { quote: "We are more often frightened than hurt; and we suffer more in imagination than in reality.",
+               author: "Seneca"
+             },
+             {
+                 quote: "If a man knows not which port he sails, no wind is favorable.",
+                 author: "Seneca"
+             },
+             {
+                 quote: "I begin to speak only when I’m certain what I’ll say isn’t better left unsaid",
+                 author: "Cato"
+             },
+             {
+                 quote: "Don’t explain your philosophy. Embody it.",
+                 author: "Epictetus"
+             },
+             {
+                 quote: "Music is the hidden arithmetical exercise of a mind unconscious that it is calculating.",
+                 author: "Leibniz"
+             },
+             {
+                 quote: "What is love? To be delighted by the happiness of another.",
+                 author: "Leibniz"
+             },
+             {
+                 quote: "In all things it is better to hope than to despair.",
+                 author: "Goethe"
+             },
+             {
+                 quote: "Magic is believing in yourself, if you can do that, you can make anything happen.",
+                 author: "Goethe"
+             },
+             {
+                 quote: "Be kind, for everyone you meet is fighting a hard battle.",
+                 author: "Plato"
+             },
+             {
+                 quote: "The two mistakes of indolence and distraction must be combated by quiet work that is carried on daily without interruption; then success will certainly be achieved.",
+                 author: "The Secret of the Golden Flower"
+                 
+             },
+             {
+                 quote: "One must not content oneself with small demands but must rise to the thought that all living creatures have to be redeemed. One must not be trivial and irresponsible in heart, but must strive to make deeds prove one’s words.",
+                 author: "The Secret of the Golden Flower"
+                 
+             },      
+         ];
