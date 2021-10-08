98a948ae0d8143754b2c288294d3eacbe49895b0
hannarosenfeld <hannarosenfeld@gmail.com>
2021-10-08 13:33:23 -0400

    Add more quotes.

--- quotes.js
+++ quotes.js
@@ -49,5 +49,40 @@
                  quote: "One must not content oneself with small demands but must rise to the thought that all living creatures have to be redeemed. One must not be trivial and irresponsible in heart, but must strive to make deeds prove one’s words.",
                  author: "The Secret of the Golden Flower"
                  
-             },      
+             },
+             {
+                 quote: "One must not content oneself with small demands but must rise to the thought that all living creatures have to be redeemed. One must not be trivial and irresponsible in heart, but must strive to make deeds prove one’s wordsNo matter how thin you slice it, there will always be two sides.",
+                 author: "Spinoza"
+                 
+             },
+             {
+                 quote: "I have made a ceaseless effort not to ridicule, not to bewail, not to scorn human actions, but to understand them.",
+                 author: "Spinoza"
+                 
+             },
+             {
+                 quote: "Pain and suffering are a kind of currency passed from hand to hand until they reach someone who receives them but does not pass them on.",
+                 author: "Simone Weil"
+                 
+             },
+             {
+                 quote: "An experiment is a question which science poses to Nature, and a measurement is the recording of Nature's answer.",
+                 author: "Max Planck"
+                 
+             },
+             {
+                 quote: "We live in a world where there is more and more information, and less and less meaning.",
+                 author: "Jean Baudrillard"
+                 
+             },
+             {
+                 quote: "Never memorize something that you can look up.",
+                 author: "Albert Einstein"
+                 
+             },
+             {
+                 quote: "An expert is a person who has made all the mistakes that can be made in a very narrow field.",
+                 author: "Niels Bohr"
+                 
+             }         
          ];
